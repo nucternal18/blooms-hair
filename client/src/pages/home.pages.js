45305a33df7d7e6edd3252800e159cd8f4c3223e
A Oladipupo-Usoro <aolausoro@gmail.com>
2020-10-11 23:37:11 +0100

    added framer-motion to animate text and card components

--- client/src/pages/home.pages.js
+++ client/src/pages/home.pages.js
@@ -1,10 +1,13 @@
 // @ts-nocheck
 import React from 'react';
-import AdobeStock_53052353 from '../Assets/images/AdobeStock_53052353.jpeg';
+import { motion } from "framer-motion";
 import Lauren from '../Assets/images/lauren-fleischmann-akfxOADwNhk-unsplash.jpg';
 import HomepageContainer from '../Container/HomeContainer/HomeCard.container';
 
-
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 const Home = () => {
     return (
@@ -12,8 +15,16 @@ const Home = () => {
         className='bg-fixed w-full'
         style={{ backgroundImage: `url(${Lauren})` }}
       >
-        <div className="homepage-card w-full">
-      
+        <div className='homepage-card w-full justify-center flex flex-col items-center text-2xl sm:text-6xl text-white font-bold'>
+          <motion.div
+            className='opacity-75'
+            initial='hidden'
+            animate='visible'
+            variants={variants}
+            transition={{ duration: 2.0 }}
+          >
+            <p>Welcome to Blooms Hair</p>
+          </motion.div>
         </div>
         <HomepageContainer />
       </section>
